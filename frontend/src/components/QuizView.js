23e24188cd0c297cf26235dc5653d6603dc24723
Paulo Neves <paulo@psneves.com.br>
2024-11-12 00:07:19 -0300

    Complete frontend TODOs

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -21,7 +21,7 @@ class QuizView extends Component {
 
   componentDidMount() {
     $.ajax({
-      url: `/categories`, //TODO: update request URL
+      url: `/categories`, //methods defined in flask backend as expected by frontend
       type: 'GET',
       success: (result) => {
         this.setState({ categories: result.categories });
@@ -49,7 +49,7 @@ class QuizView extends Component {
     }
 
     $.ajax({
-      url: '/quizzes', //TODO: update request URL
+      url: '/quizzes', //methods defined in flask backend as expected by frontend
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',
